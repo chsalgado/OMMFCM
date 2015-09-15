4e44abc2c8d32e16c7290214b1eff4bc617389d6
lalaufresa <lalaufresa@gmail.com>
2015-09-15 17:42:45 -0500

    esqueleto de angular

---
+++ Clientes/Web/privado/controladores/controladorDashboard.js
@@ -0,0 +1,5 @@
+'use strict';
+app.controller('controladorDashboard', ['$scope', '$location', '$routeParams', 'servicioDashboard', function($scope, $location, $routeParams, servicioDashboard){
+
+
+}]);
