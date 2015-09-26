75a46af1384f1273da73fc01206c66d77ad4d6d3
xxFinalMikexx <xxfinalmikexx@gmail.com>
2015-09-26 16:10:05 -0500

    Formulario de ubicación básico. Permite detectar si ubicación está o no activa y activarla en caso de respuesta negativa

--- Clientes/Apps/Android/ommfcm/app/src/main/java/com/example/george/ommfcm/ActividadFormulario.java
+++ Clientes/Apps/Android/ommfcm/app/src/main/java/com/example/george/ommfcm/ActividadFormulario.java
@@ -1,16 +1,125 @@
 package com.example.george.ommfcm;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.Location;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
 
-public class ActividadFormulario extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+public class ActividadFormulario extends ActionBarActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener {
+    private static final int RESULT_LOAD = 1;
+    private AlertDialog alert = null;
+    protected TextView coordenadas;
+
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_formulario);
+
+        getGPS();
+    }
+
+    protected void getGPS() {
+        LocationManager locationManager;
+        String context = Context.LOCATION_SERVICE;
+        locationManager = (LocationManager)getSystemService(context);
+        coordenadas = (TextView) findViewById(R.id.textTest);
+
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            configuracionGPS();
+        }
+        buildGoogleApiClient();
+    }
+    private void configuracionGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("El sistema GPS esta desactivado, ¿Desea activarlo?")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        alert = builder.create();
+        alert.show();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks((ConnectionCallbacks) this)
+                .addOnConnectionFailedListener((OnConnectionFailedListener) this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            coordenadas.setText(String.valueOf(mLastLocation.getLatitude()) + " - " + mLastLocation.getLongitude());
+        } else {
+            coordenadas.setText("No se detectaron coordenadas");
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        coordenadas.setText("No se pudieron obtener las coordenadas");
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        coordenadas.setText("Conexión suspendida");
+        mGoogleApiClient.connect();
+    }
+
+    public void regresarPrincipal(View view) {
+        Intent principalIntend = new Intent(this, ActividadPrincipal.class);
+        startActivityForResult(principalIntend, RESULT_LOAD);
+        finish();
     }
 
     @Override
