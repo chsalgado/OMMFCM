80b975f76ffc648995557ae5794dd2f8a1e59812
xxFinalMikexx <xxfinalmikexx@gmail.com>
2015-09-26 16:42:19 -0500

    Comentarios y descripción de los métodos y funciones usadas

--- Clientes/Apps/Android/ommfcm/app/src/main/java/com/example/george/ommfcm/ActividadFormulario.java
+++ Clientes/Apps/Android/ommfcm/app/src/main/java/com/example/george/ommfcm/ActividadFormulario.java
@@ -45,19 +45,31 @@ public class ActividadFormulario extends AppCompatActivity implements
         getGPS();
     }
 
+    /**
+     * Verifica si la ubicación está activa.
+     */
     protected void getGPS() {
+        //Administrador de la localización. Obtiene si el servicio de ubicación está activo.
         LocationManager locationManager;
         String context = Context.LOCATION_SERVICE;
         locationManager = (LocationManager)getSystemService(context);
+
         coordenadas = (TextView) findViewById(R.id.textTest);
 
+        //Si la localización está desactivada, muestra una alerta para activarlo.
         if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
             configuracionGPS();
         }
+        //Una vez inicializado el servicio de ubicación, se inicia el servicio de Google
         buildGoogleApiClient();
     }
+
+    /**
+     * Muestra una alerta solicitando la activacion del servicio de ubicación
+     */
     private void configuracionGPS() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //Construye la alerta. Se coloca mensaje, botón para confirmación, y cancelación.
         builder.setMessage("El sistema GPS esta desactivado, ¿Desea activarlo?")
                 .setCancelable(false)
                 .setPositiveButton("Si", new DialogInterface.OnClickListener() {
@@ -70,10 +82,16 @@ public class ActividadFormulario extends AppCompatActivity implements
                         dialog.cancel();
                     }
                 });
+
+        //Crea y muestra la alerta creada.
         alert = builder.create();
         alert.show();
     }
 
+    /**
+     * Se construye el cliente que obtendrá la localización mediante los servicios de Google.
+     * Se crea únicamente cuando la localización en el dispositivo está activa
+     */
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks((ConnectionCallbacks) this)
@@ -82,21 +100,33 @@ public class ActividadFormulario extends AppCompatActivity implements
                 .build();
     }
 
+    /**
+     * Al iniciar el servicio, conecta el objeto mGoogleApiClient con los servicios de ubicación proporcionados por Google
+     */
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
+        mGoogleApiClient.connect(); //Conecta con el servicio de ubicación de Google
     }
 
+    /**
+     * Al detenerse el servicio de ubicación, desconecta el objeto creado de los servicios de Google
+     */
     @Override
     protected void onStop() {
         super.onStop();
         if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
+            mGoogleApiClient.disconnect(); //Detiene la conexión con el servicio de Google
         }
     }
+
+    /**
+     * Una vez que se conectó con los servicios de ubicación de google, obtiene las coordenadas de la posición actual
+     * @param connectionHint
+     */
     @Override
     public void onConnected(Bundle connectionHint) {
+        //Obtiene la última localización conocida del dispositivo
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
             coordenadas.setText(String.valueOf(mLastLocation.getLatitude()) + " - " + mLastLocation.getLongitude());
@@ -105,17 +135,29 @@ public class ActividadFormulario extends AppCompatActivity implements
         }
     }
 
+    /**
+     * Mensaje de error en caso de que la conexión falle
+     * @param result
+     */
     @Override
     public void onConnectionFailed(ConnectionResult result) {
         coordenadas.setText("No se pudieron obtener las coordenadas");
     }
 
+    /**
+     * Mensaje de alerta en caso de que la conexión se haya suspendido
+     * @param cause
+     */
     @Override
     public void onConnectionSuspended(int cause) {
         coordenadas.setText("Conexión suspendida");
         mGoogleApiClient.connect();
     }
 
+    /**
+     * Al seleccionar el botón de cancelar, se regresa a la actividad principal
+     * @param view
+     */
     public void regresarPrincipal(View view) {
         Intent principalIntend = new Intent(this, ActividadPrincipal.class);
         startActivityForResult(principalIntend, RESULT_LOAD);
