5a928d34c6ffa865470ba2a162232793ac548072
Juan Hernández López <JL@MacBook-Pro-de-JL.local>
2015-09-29 21:09:40 -0500

    estilo
    cambio de estilo

--- Clientes/Apps/iOS/OMMFCM/ViewController.swift
+++ Clientes/Apps/iOS/OMMFCM/ViewController.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2015 imt. All rights reserved.
 //
 
-struct Datos {
+struct Datos
+{
     static var imagen: UIImage?
     static var latitud: CLLocationDegrees?
     static var longitud: CLLocationDegrees?
+    static var municipioOrigen: String?
+    static var municipioDestino: String?
+    static var kilometros: String?
     
-    static func borrar() {
+    static var municipioOrigenTexto: String?
+    static var municipioDestinoTexto: String?
+    
+    static func borrar()
+    {
         self.imagen = nil
         self.latitud = nil
         self.longitud = nil
+        self.municipioOrigen = nil
+        self.municipioDestino = nil
+        self.kilometros = nil
+        self.municipioOrigenTexto = nil
+        self.municipioDestinoTexto = nil
     }
 }
 
@@ -22,66 +35,78 @@ import UIKit
 import AssetsLibrary
 import CoreLocation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    @IBAction func botonTomarFoto(sender: UIButton) {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    @IBAction func botonTomarFoto(sender: UIButton)
+    {
         self.mostrarCamara()
     }
     
-    @IBAction func botonSeleccionaFoto(sender: UIButton) {
+    @IBAction func botonSeleccionaFoto(sender: UIButton)
+    {
         self.mostrarGaleria()
     }
     
-    // Unwind segue
-    @IBAction func regresaAPrincipal(segue: UIStoryboardSegue) {
-        
+    func siguienteVista()
+    {
+        if Datos.latitud != nil
+        {
+            self.performSegueWithIdentifier("confirmarFoto", sender: self)
+        }
+        else
+        {
+            self.performSegueWithIdentifier("seleccionaUbicacion", sender: self)
+        }
+
     }
     
-    func revisarDatos() {
-        self.performSegueWithIdentifier("verificarDatos", sender: self)
-    }
-    
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        self.solicitarEstados()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func mostrarCamara() {
+    func mostrarCamara()
+    {
         let picker = UIImagePickerController()
         picker.sourceType = UIImagePickerControllerSourceType.Camera
         picker.delegate = self
         self.presentViewController(picker, animated: true, completion: nil)
     }
     
-    func mostrarGaleria() {
+    func mostrarGaleria()
+    {
         let picker = UIImagePickerController()
         picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         picker.delegate = self
         self.presentViewController(picker, animated: true, completion: nil)
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
         Datos.borrar()
-        //Se quita la vista: camara o galeria
+        // Se quita la vista: camara o galeria
         picker.dismissViewControllerAnimated(true, completion: nil)
         Datos.imagen = info[UIImagePickerControllerOriginalImage] as? UIImage
         
-        //obtengo coordenadas
+        // obtengo coordenadas
         let library = ALAssetsLibrary()
         let url: NSURL? = info[UIImagePickerControllerReferenceURL] as? NSURL
         if url != nil {
             library.assetForURL(url, resultBlock:
                 {
                     (asset: ALAsset!) in
-                    if asset.valueForProperty(ALAssetPropertyLocation) != nil {
+                    if asset.valueForProperty(ALAssetPropertyLocation) != nil
+                    {
                         Datos.latitud = (asset.valueForProperty(ALAssetPropertyLocation) as! CLLocation!).coordinate.latitude
                         Datos.longitud = (asset.valueForProperty(ALAssetPropertyLocation) as! CLLocation!).coordinate.longitude
-                        print(Datos.latitud)
-                        print(Datos.longitud)
+                        self.siguienteVista()
                     }
                 }, failureBlock:
                 {
@@ -90,12 +115,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 })
             
         }
-        //me voy a la siguiente vista
-        self.revisarDatos()
+        else
+        {
+            self.siguienteVista()
+        }
     }
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        picker.dismissViewControllerAnimated(true, completion: {})
-    }
-}
-
