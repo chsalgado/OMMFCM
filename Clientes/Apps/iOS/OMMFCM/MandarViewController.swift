0f5523af64876479dc12931b99310b037c959a46
Juan Hernández López <JL@MacBook-Pro-de-JL.local>
2015-09-29 21:18:11 -0500

    mandar incidente
    cambio de estilo

--- Clientes/Apps/iOS/OMMFCM/MandarViewController.swift
+++ Clientes/Apps/iOS/OMMFCM/MandarViewController.swift
@@ -7,59 +7,117 @@
 //
 
 import UIKit
+import MapKit
 
-class MandarViewController: UIViewController {
+class MandarViewController: UIViewController
+{
     @IBOutlet weak var imagen: UIImageView!
     
-    @IBAction func botonMandarFoto(sender: UIButton) {
+    @IBOutlet weak var mapa: MKMapView!
+    @IBOutlet weak var tituloSinCoordenadas: UILabel!
+    
+    @IBOutlet weak var vistaInformacion: UIView!
+    @IBOutlet weak var municipioOrigen: UILabel!
+    @IBOutlet weak var municipioDestino: UILabel!
+    @IBOutlet weak var kilometros: UILabel!
+    
+    @IBAction func botonEnviarDatos(sender: UIButton)
+    {
         self.enviarDatos()
     }    
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         self.imagen.image = Datos.imagen
-    }
-    
-    func enviarDatos() {
-        //URL del servicio y objeto sesion
-        let url = NSURL(string: "webserviceurl")    //MANU URL SERVICIO WEB
-        let sesion = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
         
-        //solicitud
-        let request = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "POST"
+        if Datos.latitud != nil
+        {
+            self.colocaPuntoEnMapa()
+            self.tituloSinCoordenadas.hidden = true
+        }
+        else
+        {
+            self.mapa.hidden = true
+        }
         
-        //datos a enviar
-        if Datos.longitud != nil {
-            let lat = Double(Datos.latitud!)
-            let long = Double(Datos.longitud!)
-            let datosFoto = UIImagePNGRepresentation(Datos.imagen!)
-            let imgb64 = datosFoto?.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
-            
-            let datos: [String: AnyObject] = ["fecha" : "", "idEspecie": "", "long": long, "lat": lat, "mpioOrigen": "", "mpioDestino": "", "km": "", "imagen": imgb64!]
-            
-            /*  Prueba que es un JSON valido
-            let valid = NSJSONSerialization.isValidJSONObject(datos)
-            print(valid)
-            */
-            
-            let datosJson = try? NSJSONSerialization.dataWithJSONObject(datos, options: .PrettyPrinted)
-            if datosJson != nil {
-                sesion.uploadTaskWithRequest(request, fromData: datosJson, completionHandler: {(data: NSData?,
-                    response: NSURLResponse?,
-                    error: NSError?) in
-                    print(data)
-                    print(response)
-                    print(error)
-                }).resume()
-                print("envio los datos")
-            } else {
-                print("error: no pudo convertir a Json")
-            }
-        } else {
-            print("no tiene coordenadas")
-            
+        if Datos.municipioDestino != nil
+        {
+            self.colocaInformacion()
+        }
+        else
+        {
+            self.vistaInformacion.hidden = true
         }
     }
     
+    func colocaPuntoEnMapa()
+    {
+        let punto = MKPointAnnotation()
+        punto.title = "Incidente"
+        punto.coordinate.latitude = Datos.latitud!
+        punto.coordinate.longitude = Datos.longitud!
+        self.mapa.addAnnotation(punto)
+        
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(punto.coordinate,regionRadius * 5.0, regionRadius * 5.0)
+        self.mapa.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func colocaInformacion()
+    {
+        self.municipioOrigen.text = Datos.municipioOrigenTexto
+        self.municipioDestino.text = Datos.municipioDestinoTexto
+        self.kilometros.text = Datos.kilometros
+    }
+    
+    func enviarDatos()
+    {
+        // URL del servicio y objeto sesion
+        let url = NSURL(string: "webserviceurl")    // TODO
+        let sesion = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        
+        // solicitud
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        
+        // datos a enviar
+        // coordenadas
+        let lat = Datos.latitud != nil ? Double(Datos.latitud!).description : ""
+        let long = Datos.longitud != nil ?  Double(Datos.longitud!).description : ""
+        
+        // informacion
+        let municipioOri = Datos.municipioOrigen != nil ? Datos.municipioOrigen : ""
+        let municipioDes = Datos.municipioDestino != nil ? Datos.municipioDestino : ""
+        let kilomts = Datos.kilometros != nil ? Datos.kilometros : ""
+        
+        // foto
+        let datosFoto = UIImagePNGRepresentation(Datos.imagen!)
+        let imgb64 = datosFoto?.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+        
+        let datos: [String: String] = ["fecha" : "", "idEspecie": "", "long": long, "lat": lat, "mpioOrigen": municipioOri!, "mpioDestino": municipioDes!, "km": kilomts!, "imagen": imgb64!, "extension": ".png"]
+        
+        /*  Prueba que es un JSON valido
+        let valid = NSJSONSerialization.isValidJSONObject(datos)
+        print(valid)
+        */
+        
+        let datosJson = try? NSJSONSerialization.dataWithJSONObject(datos, options: .PrettyPrinted)
+        if datosJson != nil {
+            sesion.uploadTaskWithRequest(request, fromData: datosJson, completionHandler: {(data: NSData?,
+                response: NSURLResponse?,
+                error: NSError?) in
+                print(data)
+                print(response)
+                print(error)
+            }).resume()
+            print("envio los datos")
+        }
+        else
+        {
+            print("error: no pudo convertir a Json")
+        }
+        
+        
+    }
 }
