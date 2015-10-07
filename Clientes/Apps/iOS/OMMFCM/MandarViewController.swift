54d4690b20f418ff4b56b136bb7c50ac3bae25b4
Juan Hernández López <JL@MacBook-Pro-de-JL.local>
2015-10-06 19:12:42 -0500

    enviar incidente
    envio de incidente

--- Clientes/Apps/iOS/OMMFCM/MandarViewController.swift
+++ Clientes/Apps/iOS/OMMFCM/MandarViewController.swift
@@ -21,6 +21,8 @@ class MandarViewController: UIViewController
     @IBOutlet weak var municipioDestino: UILabel!
     @IBOutlet weak var kilometros: UILabel!
     
+    var envioExitoso: Bool?
+    
     @IBAction func botonEnviarDatos(sender: UIButton)
     {
         self.enviarDatos()
@@ -73,51 +75,66 @@ class MandarViewController: UIViewController
     
     func enviarDatos()
     {
-        // URL del servicio y objeto sesion
-        let url = NSURL(string: "webserviceurl")    // TODO
-        let sesion = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        
-        // solicitud
-        let request = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "POST"
-        
-        // datos a enviar
-        // coordenadas
-        let lat = Datos.latitud != nil ? Double(Datos.latitud!).description : ""
-        let long = Datos.longitud != nil ?  Double(Datos.longitud!).description : ""
-        
-        // informacion
-        let municipioOri = Datos.municipioOrigen != nil ? Datos.municipioOrigen : ""
-        let municipioDes = Datos.municipioDestino != nil ? Datos.municipioDestino : ""
-        let kilomts = Datos.kilometros != nil ? Datos.kilometros : ""
-        
-        // foto
-        let datosFoto = UIImagePNGRepresentation(Datos.imagen!)
+        // Datos a enviar
+        // Foto
+        let datosFoto = UIImageJPEGRepresentation(Datos.imagen!, 0.5)
         let imgb64 = datosFoto?.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
         
-        let datos: [String: String] = ["fecha" : "", "idEspecie": "", "long": long, "lat": lat, "mpioOrigen": municipioOri!, "mpioDestino": municipioDes!, "km": kilomts!, "imagen": imgb64!, "extension": ".png"]
+        var incidente: [String: String] = ["extension": ".jpeg", "imagen": imgb64!]
         
-        /*  Prueba que es un JSON valido
-        let valid = NSJSONSerialization.isValidJSONObject(datos)
-        print(valid)
-        */
+        // Coordenadas
+        if Datos.latitud != nil && Datos.longitud != nil {
+            incidente["lat"] = Datos.latitud!.description
+            incidente["long"] = Datos.longitud!.description
+        }
+        // Municipios
+        if Datos.municipioOrigen != nil && Datos.municipioDestino != nil && Datos.kilometros != nil {
+            incidente["mpioOrigen"] = Datos.municipioOrigen
+            incidente["mpioDestino"] = Datos.municipioDestino
+            incidente["km"] = Datos.kilometros
+        }
         
-        let datosJson = try? NSJSONSerialization.dataWithJSONObject(datos, options: .PrettyPrinted)
-        if datosJson != nil {
-            sesion.uploadTaskWithRequest(request, fromData: datosJson, completionHandler: {(data: NSData?,
-                response: NSURLResponse?,
-                error: NSError?) in
-                print(data)
-                print(response)
-                print(error)
+        let datosJson = try? NSJSONSerialization.dataWithJSONObject(incidente, options: .PrettyPrinted)
+        
+        // URL del servicio y objeto sesion
+        let urlServicio = "http://148.243.51.170:8007/obsfauna/public_html/index.php/api/incidentes"
+        let url = NSURL(string: urlServicio)
+        let sesion = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        
+        // Solicitud
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.HTTPBody = datosJson
+        
+        if datosJson != nil
+        {
+            let aviso = UIAlertController(title: "Enviando...", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+            let accion = UIAlertAction(title: "Cerrar", style: UIAlertActionStyle.Default, handler: { _ in })
+            aviso.addAction(accion)
+            self.presentViewController(aviso, animated: true, completion: {})
+            
+            sesion.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+                self.presentedViewController?.dismissViewControllerAnimated(true, completion: nil)
+                let accion = UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.Default, handler: { _ in })
+                if (response as! NSHTTPURLResponse).statusCode == 200 {
+                    let respuesta = UIAlertController(title: "Enviado", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+                    respuesta.addAction(accion)
+                    self.presentViewController(respuesta, animated: true, completion: {})
+                    self.envioExitoso = true
+                } else  {
+                    let respuesta = UIAlertController(title: "Error", message: error?.description, preferredStyle: UIAlertControllerStyle.Alert)
+                    respuesta.addAction(accion)
+                    self.presentViewController(respuesta, animated: true, completion: {})
+                    self.envioExitoso = false
+                }
             }).resume()
-            print("envio los datos")
         }
         else
         {
             print("error: no pudo convertir a Json")
+            self.envioExitoso = false
         }
-        
-        
     }
 }
