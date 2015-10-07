342fd0a4df99998e491488a26191111f5a928508
lalaufresa <lalaufresa@gmail.com>
2015-10-07 17:04:15 -0500

    revision en prueba de controladorIncidentes

--- Clientes/Web/privado/pruebas/controladores/controladorIncidentes.tests.js
+++ Clientes/Web/privado/pruebas/controladores/controladorIncidentes.tests.js
@@ -93,7 +93,7 @@ describe('controlador incidentes', function(){
         $scope.obtenerEspecies();
         expect(mockServicioEspecies.obtenerEspecies).toHaveBeenCalled();
         $timeout.flush();
-        expect($scope.especies.length).not.toBe(0);
+        expect($scope.especies).toEqual([{"idEspecie":14,"nombreComun":"nombre comun0","nombreCientifico":"nombre cientifico0","created_at":"2015-09-20 03:22:44","updated_at":"2015-09-20 03:22:44"},{"idEspecie":15,"nombreComun":"nombre comun1","nombreCientifico":"nombre cientifico1","created_at":"2015-09-20 03:22:44","updated_at":"2015-09-20 03:22:44"}]);
     });
 
     it('falla al obtener todas las especies', function(){
@@ -112,7 +112,7 @@ describe('controlador incidentes', function(){
         expect($scope.regresar).toBe(true);
         expect(mockServicioIncidentes.obtenerIncidentes).toHaveBeenCalledWith(1, 10);
         $timeout.flush();
-        expect($scope.incidentes.length).not.toBe(0);
+        expect($scope.incidentes).toEqual([{"idIncidente":26,"idEspecie":15,"fecha":null,"rutaFoto":"as","long":null,"lat":null,"mpioOrigen":3,"mpioDestino":4,"km":null,"created_at":"-0001-11-30 00:00:00","updated_at":"-0001-11-30 00:00:00","rutaThumbnail":"x"},{"idIncidente":27,"idEspecie":15,"fecha":null,"rutaFoto":"as","long":null,"lat":null,"mpioOrigen":3,"mpioDestino":4,"km":null,"created_at":"-0001-11-30 00:00:00","updated_at":"-0001-11-30 00:00:00","rutaThumbnail":"dd"}]);
         expect($scope.total).toEqual(2);
         expect($scope.desde).toEqual(1);
         expect($scope.hasta).toEqual(2);
@@ -123,32 +123,38 @@ describe('controlador incidentes', function(){
 
     it('muestra mensaje de exito cuando se elimina un incidente', function(){
         cambiarExito(true);
-        $scope.eliminarIncidente(2);
-        expect(mockServicioIncidentes.eliminarIncidente).toHaveBeenCalledWith(2);
+        var idIncidente = 2;
+        $scope.eliminarIncidente(idIncidente);
+        expect(mockServicioIncidentes.eliminarIncidente).toHaveBeenCalledWith(idIncidente);
         $timeout.flush();
         expect($scope.mensaje).toBe('El incidente ha sido eliminado');
     });
 
     it('muestra mensaje de error cuando no se puede eliminar un incidente', function(){
         cambiarExito(false);
-        $scope.eliminarIncidente(2);
-        expect(mockServicioIncidentes.eliminarIncidente).toHaveBeenCalledWith(2);
+        var idIncidente = 2;
+        $scope.eliminarIncidente(idIncidente);
+        expect(mockServicioIncidentes.eliminarIncidente).toHaveBeenCalledWith(idIncidente);
         $timeout.flush();
         expect($scope.mensaje).toBe('El incidente no fue eliminado. Intentelo más tarde');
     });
 
     it('muestra mensaje de éxito cuando se modifica un incidente', function(){
         cambiarExito(true);
-        $scope.modificarIncidente(15, 2);
-        expect(mockServicioIncidentes.modificarIncidente).toHaveBeenCalledWith(15, 2);
+        var idIncidente = 15;
+        var idEspecie = 2;
+        $scope.modificarIncidente(idIncidente, idEspecie);
+        expect(mockServicioIncidentes.modificarIncidente).toHaveBeenCalledWith(idIncidente, idEspecie);
         $timeout.flush();
         expect($scope.mensaje).toBe('El incidente ha sido modificado');
     });
 
     it('muestra mensaje de error cuando no se puede modificar un incidente', function(){
         cambiarExito(false);
-        $scope.modificarIncidente(15, 2);
-        expect(mockServicioIncidentes.modificarIncidente).toHaveBeenCalledWith(15, 2);
+        var idIncidente = 15;
+        var idEspecie = 2;
+        $scope.modificarIncidente(idIncidente, idEspecie);
+        expect(mockServicioIncidentes.modificarIncidente).toHaveBeenCalledWith(idIncidente, idEspecie);
         $timeout.flush();
         expect($scope.mensaje).toBe('El incidente no fue modificado. Intentelo más tarde');
     });
