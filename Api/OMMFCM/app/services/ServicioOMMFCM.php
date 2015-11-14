32ebdfddd1142c5b472d303883391cdf866bfe5b
jorgegonzac <jorgegonzac@gmail.com>
2015-11-14 11:36:00 -0600

    Ordenar especies
    Se ordena las especies por nombre común

--- Api/OMMFCM/app/services/ServicioOMMFCM.php
+++ Api/OMMFCM/app/services/ServicioOMMFCM.php
@@ -165,13 +165,13 @@
 	    public function getEspecies($pagina, $resultados)
 	    {
 	    	// Regresar todas las especies, excepto la que tiene id=0 (es la especie default)
-	   		$especies = Especie::all();
+	   		$especies = Especie::orderBy('nombreComun', 'asc')->get();
 
 	   		// Validar que los dos parámetros de paginación fueron enviados, de lo contrario mandar todas las especies
 	   		if(!is_null($pagina) && !is_null($resultados))
 	   		{
 				Especie::resolveConnection()->getPaginator()->setCurrentPage($pagina);
-		   		$especies = Especie::where('idEspecie', '>', 0)->paginate($resultados);	   			
+		   		$especies = Especie::where('idEspecie', '>', 0)->orderBy('nombreComun', 'asc')->paginate($resultados);	   			
 	   		}
 
 	   		return $especies;	    	
